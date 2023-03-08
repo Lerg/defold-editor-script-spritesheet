50799de978e380e0a7f02a4cd12cf059ad480940
Lerg <sergey.lerg@gmail.com>
2023-03-08 13:15:12 +0100

    Rename go file to avoid conflict.

--- spritesheet_script/build.sh
+++ spritesheet_script/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-GOOS=darwin GOARCH=amd64 go build -o plugins/bin/macos/spritesheet spritesheet.go && \
-GOOS=linux GOARCH=amd64 go build -o plugins/bin/linux/spritesheet spritesheet.go && \
-GOOS=windows GOARCH=amd64 go build -o plugins/bin/win32/spritesheet.exe spritesheet.go
+GOOS=darwin GOARCH=amd64 go build -o plugins/bin/macos/spritesheet spritesheet._go && \
+GOOS=linux GOARCH=amd64 go build -o plugins/bin/linux/spritesheet spritesheet._go && \
+GOOS=windows GOARCH=amd64 go build -o plugins/bin/win32/spritesheet.exe spritesheet._go
